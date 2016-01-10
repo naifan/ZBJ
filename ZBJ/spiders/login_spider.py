537f027ec4903108f589f4475517fdc115a2a016
naifan <haokqgood@gmail.com>
2016-01-10 16:29:35 +0800

    logingin_ider 去掉空格，下一页跳转成功

--- ZBJ/spiders/login_spider.py
+++ ZBJ/spiders/login_spider.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import Spider, Rule
 from scrapy.http import Request, FormRequest
 from scrapy.utils.url import urljoin_rfc
 from scrapy.utils.response import get_base_url
@@ -9,16 +9,16 @@ from scrapy.utils.response import get_base_url
 from ZBJ.settings import *
 from ZBJ.items import ZBJItem
 
-class LoginSpiderSpider(CrawlSpider):
+class LoginSpiderSpider(Spider):
     name = 'login_spider'
     allowed_domains = ['zbj.com']
     #start_urls = ['http://task.zbj.com/success/?kw=%E7%99%BE%E5%BA%A6%E7%9F%A5%E9%81%93']
     #start_urls = ['http://u.zbj.com/task/order']
-    start_urls = ['http://task.zbj.com//4750773/']
+    start_urls = ['http://task.zbj.com/4750773/']
 
-    rules = (
-        Rule(LinkExtractor(allow=r''), ),
-    )
+    # rules = (
+        # Rule(LinkExtractor(allow=r''), ),
+    # )
 
     def __init__(self):
         self.headers = HEADER
@@ -29,49 +29,53 @@ class LoginSpiderSpider(CrawlSpider):
             yield FormRequest(url, meta = {'cookiejar': i}, \
                               headers = self.headers, \
                               cookies =self.cookies, \
-                              #callback = self.parse_item
-                              callback = self.parse_item,\
+                              callback = self.parse_item
+                              # callback = self.parse_page \
                               )#jump to login page
 
     def parse_item(self, response):
-        # content = response.body
-        # with open('url.html', 'w') as fp:
-            # fp.write(content)
         item = ZBJItem()
         item['url'] = response.url
         if ( response.xpath('//h1/text()').extract()):
-            item['title'] = response.xpath('//h1/text()').extract()
+            item['title'] = map(unicode.strip, response.xpath('//h1/text()').extract())
         else:
             item['title'] = ""
         if (response.xpath('//p[@class="bidc"]/text()').extract()):
-            item['content'] = response.xpath('//p[@class="bidc"]/text()').extract()
+            item['content'] = map(unicode.strip, response.xpath('//p[@class="bidc"]/text()').extract())
         else:
             item['content'] = ""
         yield item
+        #下一页 
         nextLink = response.xpath('//div[@class="pagination"]/ul/li/a')
-        if nextLink.xpath('text()').extract()[0] == u'\xbb':
-            nextLink = nextLink.xpath('@href').extract()[0]
+        print nextLink.xpath('text()').extract()
+        if nextLink.xpath('text()').extract()[-1] == u'\xbb':
+            nextLink = nextLink.xpath('@href').extract()[-1]
+            print "nextLink 1: " + nextLink
+            print "base_url: " + get_base_url(response)
             nextLink = urljoin_rfc(get_base_url(response), nextLink)
-            print nextLink
+            print "nextLink 2: " + nextLink
             yield Request(nextLink, \
                 headers = self.headers,\
                 cookies = self.cookies,\
                 callback = self.parse_item,
-                )       
+                )    
+               
+      
     
     def parse_page(self, response):
         page_urls = response.xpath('//div[@class="pagination"]/ul/li/a')
         for page_url in page_urls:
-            url_short = page_url.xpath('@href').extract()[0]
-            print "url_short: " + url_short
-            url = urljoin_rfc(get_base_url(response), url_short)
-            print "url: " + url
-            yield Request(url, \
-                headers = self.headers,\
-                cookies = self.cookies,\
-                callback = self.parse_item,
-                )
-        
+            if page_url.xpath('@href'):
+                url_short = page_url.xpath('@href').extract()[0]
+                print "url_short: " + url_short
+                url = urljoin_rfc(get_base_url(response), url_short)
+                print "url: " + url
+                yield Request(url, \
+                    headers = self.headers,\
+                    cookies = self.cookies,\
+                    callback = self.parse_item,
+                    )
+ 
                
                     
                 
